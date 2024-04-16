e2d49f6f4004dfd4076b9c6790cb3da45eed3302
Olha Bolshakova <otyao25@gmail.com>
2024-04-16 20:01:09 +0200

    Homework Module 7

---
+++ Module 7/M7.py
@@ -0,0 +1,120 @@
+from address_book import AddressBook, Record
+from colorama import Fore
+
+
+def errors_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IndexError:
+            print(f"{Fore.RED} [Error] {Fore.RESET} Будь ласка, введіть команду")
+        except KeyError:
+            print(f"{Fore.RED} [Error] {Fore.RESET} Будь ласка, введіть команду")
+        except ValueError:
+            print(
+                f"{Fore.RED} [Error] {Fore.RESET} Параметри введено не коректно. Для прикладу 'add Olena 0932223355 ")
+
+    return wrapper
+
+
+@errors_handler
+def parse_input(user_input):
+    tokens = user_input.lower().split()
+    command = tokens[0]
+    args = tokens[1:]
+    return command, args
+
+
+def main():
+    print("Вітаю, Ваш асистент готовий до роботи!!\n")
+    book = AddressBook()
+    print("Доступні команди:")
+    commands = [
+        "add <і`мя> <номер>",
+        "phone <і`мя>",
+        "change <і`мя> <cтарий номер> <новий номер>",
+        "all",
+        "add-birthday <ім`я> <дата>",
+        "show-birthday <ім`я>",
+        "birthdays",
+        "hello",
+        "exit"
+    ]
+    for command in commands:
+        print(f"\t{command}")
+
+    while True:
+        user_input = input(
+            "\nВведіть команду: ")
+        command, args = parse_input(user_input)
+
+        if command == "add":
+            if len(args) != 2:
+                print("Невірний формат команди. Використайте: add <і`мя> <номер>")
+            else:
+                record = Record(args[0])
+                try:
+                    record.add_phone(args[1])
+                    book.add_record(record)
+                    print(f"Контакт {args[0]} з номером {args[1]} доданий до книги.")
+                except Exception as e:
+                    print(e)
+
+        elif command == "phone":
+            if len(args) != 1:
+                print("Невірний формат команди. Використайте: phone <і`мя>")
+            else:
+                record = book.find(args[0])
+                if record is None:
+                    print(f"Контакт {args[0]} не знайдено")
+                else:
+                    print(f"Телефони для {args[0]}: {', '.join(record.phones)}")
+
+        elif command == "change":
+            if len(args) != 3:
+                print("Невірний формат команди. Використайте: change <і`мя> <cтарий номер> <новий номер>")
+            else:
+                record = book.find(args[0])
+                if record is None:
+                    print(f"Контакт {args[0]} не знайдено")
+                record.edit_phone(args[1], args[2])
+                book.update_record(record)
+                print(f"Телефон {args[1]} для {args[0]} змінено на {args[2]}")
+        elif command == "all":
+            book.show()
+        elif command == "add-birthday":
+            if len(args) != 2:
+                print("Невірний формат команди. Використайте: add-birthday <ім`я> <дата>")
+            else:
+                record = book.find(args[0])
+                if record is None:
+                    print(f"Контакт {args[0]} не знайдено")
+                else:
+                    record.add_birthday(args[1])
+                    book.update_record(record)
+                    print(f"Додано день народження для {args[0]}")
+        elif command == "show-birthday":
+            if len(args) != 1:
+                print("Невірний формат команди. Використайте: show-birthday <ім`я>")
+            else:
+                record = book.find(args[0])
+                if record is None:
+                    print(f"Контакт {args[0]} не знайдено")
+                print(record.birthday.value.date())
+
+        elif command == "birthdays":
+            print("Ближчі дні народження:")
+            for birthday in book.get_upcoming_birthdays(7):
+                print(f"{birthday['user']} - {birthday['congratulation_date']}")
+
+        elif command == "exit" or command == "close":
+            print("Программа завершена.")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        else:
+            print("Невідома команда. Спробуйте знову.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Module 7/address_book.py
@@ -0,0 +1,126 @@
+from datetime import datetime, timedelta
+from collections import UserDict
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'{self.value}'
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        if not self.is_valid_date_format(value):
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+        super().__init__(datetime.strptime(value, "%d.%m.%Y"))
+
+    def is_valid_date_format(self, value):
+        try:
+            datetime.strptime(value, "%d.%m.%Y")
+            return True
+        except ValueError:
+            return False
+
+    def __repr__(self):
+        return f'{self.value.strftime("%d.%m.%Y")}'
+
+
+class Name(Field):
+    # реалізація класу
+    pass
+
+
+class Phone(Field):
+    def __init__(self, phone):
+        if self.validate(phone):
+            super().__init__(phone)
+        else:
+            raise Exception("Номер телефону не відповідає формату")
+
+    @staticmethod
+    def validate(phonenumber):
+        return True if re.match(r"^\d{10}$", phonenumber) else False
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+        else:
+            print(f'Phone {old_phone} is not exist!')
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def find_phone(self, phone):
+        return phone if phone in self.phones else None
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
+    def __str__(self):
+        return f"Імя: {self.name.value}, телефони: {'; '.join(p.value for p in self.phones)}, дата народження: {self.birthday.value.strftime('%d.%m.%Y')}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, search_name):
+        for key, value in self.data.items():
+            if key == search_name:
+                return value
+
+    def update_record(self, record):
+        self.data[record.name.value] = record
+
+    def show(self):
+        if len(self.data) == 0:
+            print('No data exist!')
+        for record in self.data.values():
+            print(record)
+
+    def delete(self, search_name):
+        if search_name in self.data.keys():
+            del self.data[search_name]
+            print(f'User {search_name} has been deleted!')
+
+    def get_congratulation_date(self, bday: datetime.date):
+        days_delta = 0
+        if bday.weekday() in [5, 6]:
+            days_delta = 2 if bday.weekday() == 5 else 1
+
+        return bday + timedelta(days=days_delta)
+
+    def get_upcoming_birthdays(self, days_threshold):
+        today = datetime.today().date()
+
+        upcoming_birthdays = []
+        for record in self.data.values():
+            try:
+                birthday = record.birthday.value.date().replace(year=today.year)
+
+                if 0 <= (birthday - today).days <= days_threshold:
+                    upcoming_birthdays.append({
+                        'user': record.name.value,
+                        'congratulation_date': self.get_congratulation_date(birthday)
+                    })
+
+            except ValueError:
+                print(f'Некоректна дата народження для користувача {record.name.value}')
+
+        return upcoming_birthdays
