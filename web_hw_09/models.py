c05df39193a68d900e44616991b1f908a2a195fd
Olha Bolshakova <otyao25@gmail.com>
2024-11-07 11:48:58 +0100

    HW_09

---
+++ web_hw_09/models.py
@@ -0,0 +1,34 @@
+from bson import json_util
+from mongoengine import (
+    connect,
+    Document,
+    StringField,
+    ReferenceField,
+    ListField,
+    CASCADE,
+)
+
+connect(
+    db="HW_08",
+    host="mongodb+srv://Helya:OLHnah69*@helya.hl5fw.mongodb.net/?retryWrites=true&w=majority&appName=Helya",
+)
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField(max_length=150)
+    description = StringField()
+    meta = {"collection": "authors"}
+
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=60))
+    quote = StringField()
+    meta = {"collection": "quotes"}
+
+    def to_json(self, *args, **kwargs):
+        data = self.to_mongo(*args, **kwargs)
+        data["author"] = self.author.fullname
+        return json_util.dumps(data, ensure_ascii=False)
